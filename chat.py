116448f44e76265be2c016f278cd6278eab70bf3
Zeroji <zero44.e@gmail.com>
2016-02-04 19:32:55 +0100

    I'm stoopid

--- chat.py
+++ chat.py
@@ -35,13 +35,13 @@ def delete(_):
                         return
 
 
-m8b = {'It is certain', 'It is decidedly so', 'Without a doubt',
+m8b = ['It is certain', 'It is decidedly so', 'Without a doubt',
        'Yes, definitely', 'You may rely on it', 'As I see it, yes',
        'Most likely', 'Outlook good', 'Yes', 'Signs point to yes',
        'Reply hazy try again', 'Ask again later', 'Better not tell you now',
        'Cannot predict now', 'Concentrate and ask again', 'Don\'t count on it',
        'My reply is no', 'My sources say no', 'Outlook not so good',
-       'Very doubtful'}
+       'Very doubtful']
 
 
 @command('8ball', __name__, help=';8ball should I help them?', hidden=True)
